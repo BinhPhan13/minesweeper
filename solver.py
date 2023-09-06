d5acc655c110fd5cd48e04a92aa65c71955e4d60
buoi <ptbinh2003@gmail.com>
2023-09-06 10:17:01 +0700

    seperate deduct()

--- solver.py
+++ solver.py
@@ -76,51 +76,58 @@ class Solver:
         while True:
             e0 = self.__store.fetch()
             if not e0: break
-        # BASIC
-            # if mines = 0 -> open all
-            if e0.mines == 0:
-                for r,c in e0.vars_pos: self.__open(r,c)
-                return
-            # if mines = number of variables -> flag all
-            elif e0.mines == bitcnt16(e0.mask):
-                for r,c in e0.vars_pos: self.__flag(r,c)
-                return
+            if self.__try_basic(e0): break
+            if self.__try_subtract(e0): break
 
-        # SUBTRACT
-            for i in self.__store.get_overlap(e0):
-                e1 = self.__store.get_eqn(i)
-                w0 = e0.munge(e1, True)
-                w1 = e1.munge(e0, True)
+    def __try_basic(self, e0:EQN):
+        do_sth = True
+        # if mines = 0 -> open all
+        if e0.mines == 0:
+            for r,c in e0.vars_pos: self.__open(r,c)
+        # if mines = number of variables -> flag all
+        elif e0.mines == bitcnt16(e0.mask):
+            for r,c in e0.vars_pos: self.__flag(r,c)
+        else: do_sth = False
 
-                if w0 and w1:
-                    w0cnt = bitcnt16(w0)
-                    w1cnt = bitcnt16(w1)
+        return do_sth
 
-                    w0_eqn = EQN(e0.sr, e0.sc, w0, 0)
-                    w1_eqn = EQN(e1.sr, e1.sc, w1, 0)
+    def __try_subtract(self, e0:EQN):
+        for i in self.__store.get_overlap(e0):
+            e1 = self.__store.get_eqn(i)
+            w0 = e0.munge(e1, True)
+            w1 = e1.munge(e0, True)
 
-                    flag_eqn, open_eqn = None, None
-                    if w0cnt == e0.mines-e1.mines:
-                        flag_eqn, open_eqn = w0_eqn, w1_eqn
-                    elif w1cnt == e1.mines-e0.mines:
-                        flag_eqn, open_eqn = w1_eqn, w0_eqn
+            if w0 and w1:
+                w0cnt = bitcnt16(w0)
+                w1cnt = bitcnt16(w1)
 
-                    if not (flag_eqn or open_eqn): continue
+                w0_eqn = EQN(e0.sr, e0.sc, w0, 0)
+                w1_eqn = EQN(e1.sr, e1.sc, w1, 0)
+
+                flag_eqn, open_eqn = None, None
+                if w0cnt == e0.mines-e1.mines:
+                    flag_eqn, open_eqn = w0_eqn, w1_eqn
+                elif w1cnt == e1.mines-e0.mines:
+                    flag_eqn, open_eqn = w1_eqn, w0_eqn
+
+                if flag_eqn and open_eqn:
                     for r,c in flag_eqn.vars_pos: self.__flag(r,c)
                     for r,c in open_eqn.vars_pos: self.__open(r,c)
-                    return
-                # e1 is subset of e0
-                elif w0:
-                    sub_mines = e0.mines-e1.mines
-                    assert sub_mines >= 0
-                    new_eqn = EQN(e0.sr, e0.sc, w0, sub_mines)
-                    self.__store.add(new_eqn)
-                # e0 is subset of e1
-                elif w1:
-                    sub_mines = e1.mines-e0.mines
-                    assert sub_mines >= 0
-                    new_eqn = EQN(e1.sr, e1.sc, w1, sub_mines)
-                    self.__store.add(new_eqn)
+                    return True
+            # e1 is subset of e0
+            elif w0:
+                sub_mines = e0.mines-e1.mines
+                assert sub_mines >= 0
+                new_eqn = EQN(e0.sr, e0.sc, w0, sub_mines)
+                self.__store.add(new_eqn)
+            # e0 is subset of e1
+            elif w1:
+                sub_mines = e1.mines-e0.mines
+                assert sub_mines >= 0
+                new_eqn = EQN(e1.sr, e1.sc, w1, sub_mines)
+                self.__store.add(new_eqn)
+
+        return False
 
     def __open(self, row:int, col:int):
         assert self.__game.open(row,col, False)
